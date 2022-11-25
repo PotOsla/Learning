ce0458f541a4eaba53021ad690ddab8b5b9c91ec
Mikhail <smirskijj@gmail.com>
2022-11-25 12:20:03 +0300

    Katas/ReversedStrings/ finished

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -2,12 +2,6 @@
 {
     private static void Main(string[] args)
     {
-        int a = 33;
-        int b = 600;
-        byte c = checked((byte)(a + b));
-        CustomMath math = new CustomMath();
-        int result = CustomMath.multiply(2, 6);
-        Console.WriteLine(result);
-
+        ReversedStrings.Solution("gucci");
     }
 }
--- Learning/ReversedStrings.cs
+++ Learning/ReversedStrings.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 public static class ReversedStrings
 
 {
     public static string Solution(string str)
     {
-        throw new NotImplementedException("TODO: Kata.Solution(string) => string");
+        string rts = "";
+        char[] chars = str.ToCharArray();
+        Array.Reverse(chars);
+        foreach (var lit in chars)
+            rts += lit.ToString();
+        return rts;
     }
 }
