1e7af7295e7d90521de227717e7270dce9913395
Mikhail <smirskijj@gmail.com>
2022-11-25 13:06:49 +0300

    Katas/KeepHydrated/ start and finish

---
+++ KataTests/KeepHydratedTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture]
+public class KeepHydratedTests
+{
+    [Test]
+    [TestCase(2,1)]
+    [TestCase(1.4, 0)]
+    [TestCase(12.3, 6)]
+    [TestCase(0.82, 0)]
+    [TestCase(1787, 893)]
+    [TestCase(0, 0)]
+    public static void Test1(double time, int Litres)
+    {
+        Assert.AreEqual(Litres, KeepHydrated.Litres(time));
+    }
+}
---
+++ Learning/KeepHydrated.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class KeepHydrated
+{
+    public static int Litres(double time)
+    {
+        double Litres = 0.5;
+        int litintime = (int)(Math.Floor(time) * Litres);
+        return litintime;
+    }
+}
