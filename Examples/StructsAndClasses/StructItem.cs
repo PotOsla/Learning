1ff3b408329abde01d867a144c33029317b4bdff
Dmitry Kulik <liamkukid@gmail.com>
2022-11-28 15:09:56 +0100

    struct example

---
+++ Examples/StructsAndClasses/StructItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Examples.StructsAndClasses
+{
+    public struct StructItem
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public StructItem(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
