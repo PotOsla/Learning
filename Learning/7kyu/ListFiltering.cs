a61f8859093b6955abccfc8b435fde27dae54059
Mikhail <smirskijj@gmail.com>
2022-12-06 13:34:22 +0300

    Katas/7kyu started and finished

---
+++ Learning/7kyu/ListFiltering.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class ListFilterer
+{
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        var filter = new List<int>();
+        int temp;
+        foreach (var i in listOfItems)
+        {
+            if (i is string) //listOfItems.GetType() == typeof(string) не работал
+                 continue;
+            temp = (int)i;
+            filter.Add(temp);
+
+        }
+        return filter;
+    }
+}
