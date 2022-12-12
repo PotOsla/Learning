ecc9034b6f9fe6a3f90a7e932a8813dd530457b3
Mikhail <smirskijj@gmail.com>
2022-12-12 20:47:09 +0300

    7kyu/Benford start and finished

---
+++ Learning/7kyu/Benford.cs
@@ -0,0 +1,24 @@
+﻿public class Benford
+{
+    public static int[] GetBenfordStatistics(string text)
+    {
+        var statistics = new int[10];
+        string[] temp;
+        temp = text.Split(' ');
+        foreach (var word in temp)
+        {
+            foreach (var letter in word)
+            {
+                bool check = char.IsNumber(letter);
+                if (check)
+                {
+                    int numbersint = int.Parse(letter.ToString());
+                    statistics[numbersint]++;
+                    
+                }
+                break;
+            }
+        }
+        return statistics;
+    }
+}
--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,31 +1,13 @@
 ﻿
 class Program
 {
-    static void Main(string[] args)
+    static void Update(string s)
     {
-        var contacts = new List<string>();
-
-        contacts.Add("Sasha:sasha@gmail.com");
-        contacts.Add("Sancho:sancho@mail.ru");
-        contacts.Add("Roma:roma@gmail.com");
-        contacts.Add("Gusein:gusein@mail.com");
-        contacts.Add("G:gusein@mail.com");
-        contacts.Add(":");
-        contacts.Add("");
-        contacts.Add("Gusein:");
-        contacts.Add("Gu:gusein@mail.com");
-        contacts.Add("G:gusein@mail.com");
-        contacts.Add("Roma:4567@gmail.com");
-        contacts.Add("Sasha:sa@gm.com");
-        contacts.Add(":gusein@mail.com");
-        contacts.Add("Roma:");
-        contacts.Add($"{null}:sa@gm.com:priz@rty.com");
-        contacts.Add($"{null}:{null}");
-        contacts.Add($"Sasha:{null}");
-        contacts.Add(":gusein@mail.com");
-        contacts.Add("Roma:");
-
-        UsefulAcquaintances.OptimizeContacts(contacts);
+        s = "adc";
+    }
+    static void Main()
+    {
+        Benford.GetBenfordStatistics("Ekibastuz GRES-Two Power Station 420");
     }
 
 }
